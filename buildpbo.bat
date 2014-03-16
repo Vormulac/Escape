3ec6791d838e832feb99c1fb7b93d2848f339cfa
NeoArmageddon <neoarmageddon@web.de>
2014-03-17 00:55:02 +0100

    Again

--- buildpbo.bat
+++ buildpbo.bat
@@ -11,7 +11,7 @@ echo BRANCH: %GIT_BRANCH% >> %WORKSPACE%\version.txt
 echo BUILD INFO: %BUILD_URL% >> %WORKSPACE%\version.txt
 
 echo Copied over folder to binned folder
-"C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\checkout\escape.altis" "%WORKSPACE%" -PROJECT "%WORKSPACE%\checkout\escape.altis" -PACK
+"C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\checkout\escape.altis" "%WORKSPACE%" -PROJECT "%WORKSPACE%\checkout\escape.altis"
 echo Created PBOs
 ::forfiles /p %WORKSPACE%\..\%JOB_NAME%_binned /c "cmd /c if @ISDIR==TRUE rmdir /s /q @PATH"
 ::echo Removed binned folders
