4e63ad943e9b6e9120982b6c769f5926d4f0caf9
NeoArmageddon <neoarmageddon@web.de>
2014-03-17 16:09:16 +0100

    Added automatic mirroring the builded pbo to webserver

--- buildpbo.bat
+++ buildpbo.bat
@@ -14,6 +14,8 @@ echo Copied over folder to binned folder
 ::"C:\Steam\SteamApps\common\Arma 3 Tools\FileBank\FileBank.exe" -dst "%WORKSPACE%" source "%WORKSPACE%\checkout\escape.altis"
 "C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\checkout\escape.altis" "%WORKSPACE%" -DEBUG -PREFIX ""
 echo Created PBOs
+xcopy "%WORKSPACE%\escape.altis.pbo" "C:\Server\Apache2.4\htdocs\escape.anzp.de\escape_build%BUILD_NUMBER%.altis.pbo"
+echo PBO copied to webserver
 ::forfiles /p %WORKSPACE%\..\%JOB_NAME%_binned /c "cmd /c if @ISDIR==TRUE rmdir /s /q @PATH"
 ::echo Removed binned folders
 if not exist %WORKSPACE%\escape.altis.pbo exit /b 1
