0d4a3db78ce42eb35364427aca31dda3d7757bd2
NeoArmageddon <neoarmageddon@web.de>
2014-03-17 17:15:42 +0100

    Added include/build.hpp

--- buildpbo.bat
+++ buildpbo.bat
@@ -10,6 +10,9 @@ echo COMMIT: %GIT_COMMIT% >> %WORKSPACE%\version.txt
 echo BRANCH: %GIT_BRANCH% >> %WORKSPACE%\version.txt
 echo BUILD INFO: %BUILD_URL% >> %WORKSPACE%\version.txt
 
+del %WORKSPACE%\checkout\escape.altis\include\build.hpp /f /q
+echo #define BUILD %BUILD_NUMBER% >> %WORKSPACE%\checkout\escape.altis\include\build.hpp
+
 echo Copied over folder to binned folder
 ::"C:\Steam\SteamApps\common\Arma 3 Tools\FileBank\FileBank.exe" -dst "%WORKSPACE%" source "%WORKSPACE%\checkout\escape.altis"
 "C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\checkout\escape.altis" "%WORKSPACE%" -DEBUG -PREFIX ""
